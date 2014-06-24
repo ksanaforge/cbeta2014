9360b280eb11809e6f64924afa8145f7ed195ef9
yapcheahshen <yapcheahshen@gmail.com>
2014-06-24 11:50:55 +0800

    212 seconds 304MB

--- mkdb.js
+++ mkdb.js
@@ -1,20 +1,25 @@
-
-var taisho="/CBReader/XML/T01/*.xml";
+var taisho="/CBReader/XML/T*/*.xml";
+var outback = function (s) {
+    while (s.length < 70) s += ' ';
+    var l = s.length; 
+    for (var i = 0; i < l; i++) s += String.fromCharCode(8);
+    process.stdout.write(s);
+}
 var beforebydstart=function(s,status) {
-	console.log("start",s.length);
+	//console.log("start",s.length);
 }
 var afterbodyend=function(s,status) {
-	console.log("end",s.length);
+	//console.log("end",s.length);
 }
 
 var onFile=function(fn) {
-	console.log("indexing",fn)
+	outback("indexing"+fn);
 }
-/*
-  output filename
-  backup filename
-*/
-module.exports={
+var finalized=function(session) {
+	console.log("VPOS",session.vpos);
+	console.log("FINISHED")
+}
+var config={
 	name:"cbeta"
 	,config:"simple1"
 	,glob:taisho
@@ -22,9 +27,15 @@ module.exports={
 	,format:"TEIP5"
 	, bodystart: "<body>"
 	, bodyend : "</body>"
+	,reset:true
+	,finalized:finalized
 	,callbacks: {
-		beforebodystart:beforebydstart
-		,afterbodyend:afterbodyend
-		,onFile:onFile
+		//beforebodystart:beforebydstart
+		//,afterbodyend:afterbodyend
+		onFile:onFile
 	}
 }
+setTimeout(function(){ //this might load by gulpfile-app.js
+	if (!config.gulp) require("ksana-document").build();
+},100)
+module.exports=config;
