e3d4b9d1b449fed75b5bcfeeaacf220b36fd9980
yapcheahshen <yapcheahshen@gmail.com>
2014-06-30 17:58:38 +0800

    tei parser rewrite

--- cbeta_tei_p5.js
+++ cbeta_tei_p5.js
@@ -1,43 +1,47 @@
 var sax=require("sax");
-var apparatus=require("./apparatus");
-var choice=require("./choice");
-var note=require("./note");
-var errors=[];
+
+var errors=[],anchors=[];
 var parser=null,filename="";
-var apps=[], notes=[] ,choices=[];
+var context=null;
+
 var warn=function(err) {
 	errors.push(err, filename);
-	console.log(err,filename);
+	//console.log(err,filename);
 }
-var onclosetag_app=function(app) {
-	apps.push(app);
-	parser.onclosetag=onclosetag;
-	parser.onopentag=onopentag;
-}
-var onclosetag_note=function(note) {
-	notes.push(note);
-	parser.onclosetag=onclosetag;
-	parser.onopentag=onopentag;
-}
-var onclosetag_choice=function(choice) {
-	choices.push(choice);
-	parser.onclosetag=onclosetag;
-	parser.onopentag=onopentag;
+var ontext=function(e) {
+	if (context.handler) context.text+=e;
 }
 var onopentag=function(e) {
-	if (e.name=="app") {
-		apparatus.open(e.attributes,parser, onclosetag_app);
-	} else if (e.name=="cb:tt") {
-		apparatus.opencbtt(e.attributes,parser, onclosetag_app);
-	} else if (e.name=="note") {
-		if (e.attributes.n) note.open(e.attributes,parser, onclosetag_note);
-	} else if (e.name=="choice") {
-		choice.open(e.attributes,parser, onclosetag_choice);
+	context.paths.push(e.name);
+	context.parents.push(e);
+	context.now=e;	
+	context.path=context.paths.join("/");
+	if (!context.handler) {
+		var handler=context.handlers[context.path];
+		if (handler) {
+			context.handler=handler;
+		}		
 	}
+	if (context.handler)  context.handler();
 }
 
 var onclosetag=function(e) {
+	context.now=context.parents[context.parents.length-1];
 
+	var handler=context.handlers[context.path];
+	if (handler) {
+		if (context.handler) context.handler(true);
+		context.handler=null;//stop handling
+		context.text="";
+	} else if (context.handler) {
+		context.handler(true);
+	}
+	context.paths.pop();
+	context.parents.pop();
+	context.path=context.paths.join("/");		
+}
+var addHandler=function(path,handler) {
+	if (handler) context.handlers[path]=handler;
 }
 var closeAnchor=function(pg,T,anchors,id,texts) {
 	var beg="beg"+id.substr(3);
@@ -75,90 +79,58 @@ var createAnchors=function(parsed) {
 	}
 	return anchors;	
 }
-var resolveApp=function(apps,anchors,texts) { //resolve app and cb:tt
+
+var resolveChoice=function(choices,anchors,texts) {
 	var froms={};
-	apps.map(function(A,idx){ 
-		if (!A.from) {
-			warn("no 'from' in "+JSON.stringify(A));
+	choices.map(function(C,idx){ 
+		if (!C.from) {
+			warn("no 'from' in "+JSON.stringify(C));
 			return;
 		} 
-		if (froms[A.from]) warn("repeat id"+A.from);
-		froms[A.from]=idx+1;
+		if (froms[C.from]) warn("repeat id"+C.from);
+		froms[C.from]=idx+1;
 	});
 
 	for (var i=0;i<anchors.length;i++) {
 		var id=anchors[i][3];
-		if (typeof id!="string") continue;//already resolve
+		var link=anchors[i][4] || [];
 		if (froms[id]) {
-			var rdg=apps[ froms[id]-1].rdg;
-			var lemma=apps[ froms[id]-1].lemma.replace(/[\n\t]/g,"");
+			var sic=choices[ froms[id]-1].sic;
+			var corr=choices[ froms[id]-1].corr.replace(/[\n\t]/g,"");
 			var sourcetext=texts[anchors[i][0]].t.substr( anchors[i][1], anchors[i][2]).replace(/[\n\t]/g,"");
 			//beg0034031 , inline note, sourcetext is ""
-			if (lemma!=sourcetext && lemma &&sourcetext) console.log("lemma not same"+JSON.stringify(lemma+"<>"+sourcetext+" id:"+id));
-			anchors[i][3]={type:"app", rdg:rdg};
+			if (corr!=sourcetext && corr &&sourcetext) console.log("corr not same"+JSON.stringify(corr+"<>"+sourcetext+" id:"+id));
+			link.push({type:"choice", sic:sic});
 		}
+		anchors[i][4]=link;
 	}
-}
-var resolveNote=function(notes,anchors) {
-	var targets={};
-	notes.map(function(N,idx){ 
-		if (!N.target) {
-			warn("no 'target' in "+JSON.stringify(N));
-			return;
-		} 
-		/*
-		t01n0026_009.xml
-		<note n="0478005" place="foot" type="cf." target="#nkr_note_cf._0478005">[Nos. 33-35]</note>
-		<note n="0478005" place="foot" type="cf." target="#nkr_note_cf._0478005">cf. Vinaya, Cu IX.1.4<
-		*/
-		if (targets[N.target]) { //multiple <note> share same target
-			if (typeof targets[N.target]=="number")  targets[N.target]=[targets[N.target]];
-			targets[N.target].push(idx+1);
-		}else {
-			targets[N.target]=idx+1;	
-		}
-	});
-
-	for (var i=0;i<anchors.length;i++) {
-		var id=anchors[i][3];
-		if (typeof id!="string") continue;//already resolve
-		if (targets[id]) {
-			if (typeof targets[id]=="number") {
-				var text=notes[ targets[id]-1].text;	
-			} else { //joining multiple note
-				var text="";
-				for (var j=0;j<targets[id].length;j++) {
-					text+=notes[targets[id][j]-1].text+'\n';
-				}
-			}
-			anchors[i][3]={type:"note", text:text};
-		}
-	}
-}
-var resolveChoice=function() {
-	// i am tired...
 }
 var  createMarkups=function(parsed) {
-	var anchors=createAnchors(parsed);
-	resolveApp(apps,anchors,parsed.texts);
-	resolveNote(notes,anchors);
-	resolveChoice(choices,anchors);
+
+	anchors=createAnchors(parsed);
+	//resolveApp(apps,anchors,parsed.texts);
+	//resolveCbtt(cbtt,anchors,parsed.texts);
+	require("./note").resolve(anchors);
+	//resolveChoice(choices,anchors,parsed.texts);
+
 	for (var i=0;i<anchors.length;i++) {
-		if (typeof anchors[i][3]=="string") {
+		if (anchors[i][4] && !anchors[i][4].length) {
 			warn("unresolve "+anchors[i]);
 		}
 	}
 	return anchors;
 }
 
+
 var parseP5=function(xml,parsed,fn) {
 	parser=sax.parser(true);
-	apps=[];
-	notes=[];
-	choices=[];
 	filename=fn;
+	context={ paths:[] , parents:[], handlers:{}, text:"" ,now:null};
 	parser.onopentag=onopentag;
 	parser.onclosetag=onclosetag;
+	parser.ontext=ontext;
+	addHandler(  "TEI/text/back/cb:div/p/note", require("./note").handler );
+
 	parser.write(xml);
 
 	parser=null;
--- note.js
+++ note.js
@@ -1,42 +1,62 @@
+var notes=[];
 var note=null;
-var innote=false;
-var sictext="",corrtext="";
-var closenote=null;
-var parser=null;
-var ontext=function(t) {
-	note.text+=t;
-}
-var onclosechoice=function(choice) {
-/*
-  <choice> inside note has no "from" and "to",
-  use <corr> directly in node text,  <sic> is dropped.
-*/
-
-	if (choice.corr) note.text+=choice.corr;  
-	parser.onopentag=onopentag;
-	parser.onclosetag=onclosetag;
-	parser.ontext=ontext;
-}
-var onopentag=function(e) {
-	if (e.name=="choice") { //cbeta wit always wit1
-		require("./choice").open(e,parser,onclosechoice);
+var handler=function(ending) {
+	var node=this.now.name;
+	var target=this.now.attributes.target;
+	if (ending) {
+		if (node=="note") {
+			note.text=this.text;
+			notes.push(note);			
+		} else if (node=="choice") {
+			console.log("ending choice")
+		}
+	} else {
+		if (this.now.name=="note") {
+			if (target) note={target:target.substr(1),text:""};
+			else this.handler.null; //note without target is dropped
+		} else if (this.now.name=="choice") {
+			console.log(this.now)
+		}
 	}
 }
-var onclosetag=function(e) {
-	if (e=="note") {
-		closenote(note);
-	} else if (e=="") {
-		innote=false;
+
+var resolve=function(anchors) {
+	console.log(notes);
+	return;
+	var targets={};
+	notes.map(function(N,idx){ 
+		if (!N.target) {
+			warn("no 'target' in "+JSON.stringify(N));
+			return;
+		} 
+		/*
+		t01n0026_009.xml
+		<note n="0478005" place="foot" type="cf." target="#nkr_note_cf._0478005">[Nos. 33-35]</note>
+		<note n="0478005" place="foot" type="cf." target="#nkr_note_cf._0478005">cf. Vinaya, Cu IX.1.4<
+		*/
+		if (targets[N.target]) { //multiple <note> share same target
+			if (typeof targets[N.target]=="number")  targets[N.target]=[targets[N.target]];
+			targets[N.target].push(idx+1);
+		}else {
+			targets[N.target]=idx+1;	
+		}
+	});
+
+	for (var i=0;i<anchors.length;i++) {
+		var id=anchors[i][3];
+		var link=anchors[i][4] || [];
+		if (targets[id]) {
+			if (typeof targets[id]=="number") {
+				var text=notes[ targets[id]-1].text;	
+			} else { //joining multiple note
+				var text="";
+				for (var j=0;j<targets[id].length;j++) {
+					text+=notes[targets[id][j]-1].text+'\n';
+				}
+			}
+			link.push({type:"note", text:text});
+		}
+		anchors[i][4]=link;
 	}
 }
-var open=function(e,_parser,_closenote){
-	if (!e.target) return;
-	note={target:e.target.substr(1),text:""};
-	parser=_parser;
-	parser.ontext=ontext;
-	closenote=_closenote;
-	parser.onopentag=onopentag;
-	parser.onclosetag=onclosetag;
-}
-
-module.exports={open:open}
+module.exports={handler:handler, resolve:resolve};
