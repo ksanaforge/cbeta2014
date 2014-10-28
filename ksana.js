100408a4d1d9cd5cdca2cca56aa4b96a27738eda
yapcheahshen <yapcheahshen@gmail.com>
2014-10-28 23:49:22 +0800

    change to ksana.js

---
+++ ksana.js
@@ -0,0 +1,23 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "CBETA大正藏",
+ "date": "2014-10-26T16:36:31.938Z",
+ "description": "cbeta taisho tipitaka",
+ "files": [
+  "index.html",
+  "build.js",
+  "build.css",
+  "jquery.js",
+  "react-with-addons.js",
+  "cbeta.kdb"
+ ],
+ "filedates": [
+  "2014-10-18T15:31:09.000Z",
+  "2014-10-26T15:12:16.000Z",
+  "2014-10-26T15:12:16.000Z",
+  "2014-07-29T06:55:41.000Z",
+  "2014-07-29T14:51:20.000Z",
+  "2014-10-23T06:56:26.000Z"
+ ]
+})
