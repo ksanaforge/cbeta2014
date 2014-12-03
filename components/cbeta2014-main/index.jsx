adc289aaf4dc274e68f3d669ba815d5b0103581c
yap cheah shen <yapcheahshen@gmail.com>
2014-12-04 01:16:16 +0800

    search dingfubao

--- components/cbeta2014-main/index.jsx
+++ components/cbeta2014-main/index.jsx
@@ -1,11 +1,13 @@
 var tofindExtra=function() {
   return <a href="#" onClick={this.dosearch}>給孤獨</a>
 }
+ 
 var Main = React.createClass({
   mixins:[Require("defaultmain")],
-  tocTag:"mulu",
+  tocTag:"mulu",  
   defaultTofind:"正觀",
   tofindExtra:tofindExtra,
   dbid:"cbeta",
+  dictionaries:["dingfubao_dict"]
 });
 module.exports=Main;
